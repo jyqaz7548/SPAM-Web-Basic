46359fd1bcd390dded86c6ec08df7ca50c871b20
jyqaz7548 <24robot10507@sonline20.sen.go.kr>
2024-04-11 19:07:03 +0900

    study

---
+++ js/var.js
@@ -0,0 +1,30 @@
+let name = "유준영";
+let age = 17;
+let height = 190;
+let gf = false;
+let bf = undefined;
+let baby = null;
+
+//console.log(name);
+//console.log(age);
+//console.log(height);
+//console.log(gf);
+//console.log(bf);
+//console.log(baby);
+
+//제 이름은 name이고,19살에 여자친구는 false 남자친구는 undefined, 애기는 Null입니다.
+console.log(
+  "제 이름은 " +
+    name +
+    "이고, " +
+    age +
+    "살에 키는 " +
+    height +
+    ",여자친구는 " +
+    gf +
+    " 남자친구는 " +
+    bf +
+    ",애기는 " +
+    baby +
+    "입니다. "
+);
